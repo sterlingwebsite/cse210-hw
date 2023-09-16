ee055cea0f44dba8a092253ba7191260b1d88861
sterlingwebsite <smsteele14@gmail.com>
2023-09-15 22:07:54 -0600

    completed assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade_percentage_string = Console.ReadLine();
+        int grade_percentage = int.Parse(grade_percentage_string);
+        string letter = "";
+        if (grade_percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade_percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade_percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade_percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}.");
+        if (grade_percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed.");
+        }
+        else
+        {
+            Console.WriteLine("You'll do better next time.");
+        }
     }
 }
