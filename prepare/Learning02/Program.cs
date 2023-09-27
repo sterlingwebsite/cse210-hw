2257ec862499e503ebb27722790bfb5f436907c2
sterlingwebsite <smsteele14@gmail.com>
2023-09-27 14:05:21 -0600

    completed w3 prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2019;
+        Job job2 = new Job();
+        job2._jobTitle = "MacOs Specialist";
+        job2._company = "Apple";
+        job2._startYear = 2019;
+        job2._endYear = 2023;
+        Resume myResume = new Resume();
+        myResume._name = "Sterling Steele";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
