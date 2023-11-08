35833584a4fbeb22c0e33346020f40bd2322916a
Sterling Steele <smsteele14@gmail.com>
2023-11-07 17:21:37 -0700

    update

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,18 +1,5 @@
 public class BreathingActivity : Activity
 {
-    private void RunSpinner(int durationInSeconds)
-    {
-        string[] spinner = { "-", "\\", "|", "/" };
-        int index = 0;
-        for (int i = 0; i < durationInSeconds * 10; i++) // Assuming each spin takes 0.1 seconds
-        {
-            Console.Write($"\r{spinner[index]}"); // Use \r to overwrite the spinner in the same line
-            Thread.Sleep(100); // Wait for 0.1 seconds
-            index = (index + 1) % spinner.Length; // Move to the next spinner character
-        }
-        Console.WriteLine(); // Move to the next line after the spinner is done
-    }
-
     private void RunCountdown6(int durationInSeconds2)
     {
         string[] countdown = { "6", "5", "4", "3", "2", "1"};
