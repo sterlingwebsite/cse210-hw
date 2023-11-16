f0b275281c7bdf6152f0810d6d339ac110a1f35c
sterlingwebsite <smsteele14@gmail.com>
2023-11-15 18:25:52 -0700

    updates

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -22,7 +22,27 @@ class Program
 
         if (_choiceInt == 1)
         {
-
+            // Offer to create a new goal for the user. the new goal is saved temporarily in a list called _goals.
+        }
+        else if (_choiceInt == 2)
+        {
+            // Offer to list the goals created by the user, saved in the list: _goals
+        }
+        else if (_choiceInt == 3)
+        {
+            // Offer to save the list of goals in a file. Ask for the filname
+        }
+        else if (_choiceInt == 4)
+        {
+            // Offer to load goals from a file and save them to the _goals list. Ask for the filename, and load the goals.
+        }
+        else if (_choiceInt == 5)
+        {
+            // Offer to record an event for the user. Compare the event to the list of _goals and adjust the list accordingly.
+        }
+        else if (_choiceInt == 6)
+        {
+            // Quit the program
         }
     }
 }
